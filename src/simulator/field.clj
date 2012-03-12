947b4f1a189bda79d3552b05b8a0bd7aa6f4524b
Jay Vyas Peerindex Dev Setup <jayunit100@gmail.com>
2012-03-11 19:43:13 -0500

    Added comments to the proxy methods / swing stuff

--- src/simulator/field.clj
+++ src/simulator/field.clj
@@ -31,11 +31,17 @@
   [g position cells color]
   (doall (map #(draw-box g position (x %1 ) (y %1) color) cells) ))
 
+;;Returns true iff a cell is visible.
+;;That is... true if x coordinate of a cell is < col+2, and y coord < rows + 2
 (defn visible?
   [rows columns cell]
   (and (< (x cell) (+ columns 2)) (< (y cell) (+ rows 2))))
 
-
+;;This is the true "starting" point for the application.
+;;inputs are 
+;;(1)  
+;;(2) 
+;;(3) A map of options.
 (defn field-panel 
   [engine seed options]
   (let [columns (options :columns)
@@ -43,15 +49,18 @@
         position  #(* %1 (options :cellsize))
         new-cells (atom seed)
         previous-cells (atom ())
+	;;"partial": We can now call visible with only one argument - the cell. 
         visible-cell? (partial visible? rows columns)
-        panel (proxy [JPanel ActionListener] []
+        ;;Bind the "panel" to a proxy JPanel implementing ActionListener
+	panel (proxy [JPanel ActionListener] []
+		;;Override paintComponent. 
                 (paintComponent [g]
                   (let [removed (set/difference @previous-cells @new-cells)
                         added (set/difference @new-cells @previous-cells)]
                     (draw-cells g position removed Color/WHITE)
                     (draw-cells g position added Color/BLACK)
                     (draw-grid g columns rows position)))
-                
+		;;Implement the mandatory actionPerformed method of ActionListener.
                 (actionPerformed [e]
                   (swap! previous-cells (fn [_] (deref new-cells)))
                   (swap! new-cells 
@@ -70,10 +79,14 @@
     .show))
 
 ;;Arguments are the 'engine' and the 'seed' 
+;;This is a polymorphic function - the function is dispatched 
+;;via the various implementations. 
 (defn run-game 
+  ;;This one takes 2 args, the engine + seed.
   ([engine seed]  
     (run-game engine seed  
       {:columns 50 :rows 50 :speed 500 :cellsize 10}))
+  ;;This one takes 3 args, an options map as the 3rd arg.
   ([engine seed options] 
     (let [panel (field-panel engine seed options)
           frame (field-frame panel)
