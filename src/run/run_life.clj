14602a0829ac803ea7e2e34ac4c7c21976cb089b
Colbert Sesanker <sesanker0@gmail.com>
2012-03-15 14:44:10 -0400

    life function runs game

---
+++ src/run/run_life.clj
@@ -0,0 +1,8 @@
+(ns run.run_life
+  (:use [engine.game])
+  (:use [simulator.field]))
+
+(defn life [game]
+ "Choose between games: box, boat, blinker, glider and gosper-glider-gun"
+  (run-game tick game))
+
